db9344fd40204151ac22876b9760cea5f61f8ec8
sakaimidori <mdr20150526@gmail.com>
2022-10-07 11:13:48 +0000

    add:test

---
+++ test/controllers/cafes/menus_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Cafes::MenusControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/user/cafes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class User::CafesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
