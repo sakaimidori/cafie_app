f9327a555678595b0dd13c0ec8b7d7e49760d599
sakaimidori <mdr20150526@gmail.com>
2022-10-16 17:33:37 +0000

    update:Favoriteモデル,Cafeモデル,Userモデルのアソシエーション

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -6,6 +6,7 @@ class Cafe < ApplicationRecord
 
 
   has_many :menus, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
                             #営業時間外　　　　空席あり　　　残りわずか　　　空席なし
    enum congestion_status: { outside_hours: 0, available: 1, little_left: 2, not_available: 3 }
@@ -13,6 +14,11 @@ class Cafe < ApplicationRecord
   #緯度経度を取得する
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+
+  #ユーザidがFavoritesテーブル内に存在するかを調べる
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 
 
