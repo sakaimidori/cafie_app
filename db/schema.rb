fafeb734ec69a2f64c89b1cc8d27affc53e2edf6
sakaimidori <mdr20150526@gmail.com>
2022-10-16 17:18:30 +0000

    update:favoriteテーブルをデータベースへ反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_062609) do
+ActiveRecord::Schema.define(version: 2022_10_16_170645) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +63,13 @@ ActiveRecord::Schema.define(version: 2022_10_06_062609) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_cafes_on_email", unique: true
     t.index ["reset_password_token"], name: "index_cafes_on_reset_password_token", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "cafe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "menus", force: :cascade do |t|
