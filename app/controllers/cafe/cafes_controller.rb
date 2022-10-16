e55f6206fd94e78583daea3c311458acdfca05ec
sakaimidori <mdr20150526@gmail.com>
2022-10-16 13:40:16 +0000

    update:ストロングパラメータにcongestion_statusカラムを追加

--- app/controllers/cafe/cafes_controller.rb
+++ app/controllers/cafe/cafes_controller.rb
@@ -40,7 +40,7 @@ class Cafe::CafesController < ApplicationController
   private
 
   def cafe_params
-    params.require(:cafe).permit(:cafe_name, :phone_number, :address, :email, :business_hour, :fixed_holiday, :payment_method, :smoking, :introduce, :remarks)
+    params.require(:cafe).permit(:cafe_name, :phone_number, :address, :email, :business_hour, :fixed_holiday, :payment_method, :smoking, :introduce, :remarks, :latitude, :longitude, :congestion_status)
   end
 
 end
