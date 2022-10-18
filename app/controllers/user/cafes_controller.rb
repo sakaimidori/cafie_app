c8c69722d0837f139befe17ba87263c13dcec6e6
sakaimidori <mdr20150526@gmail.com>
2022-10-18 16:09:40 +0000

    update:お気に入り機能のcreate、destroyアクションの中身を実装

--- app/controllers/user/cafes_controller.rb
+++ app/controllers/user/cafes_controller.rb
@@ -7,6 +7,19 @@ class User::CafesController < ApplicationController
   def show
     @cafe = Cafe.find(params[:id])
     gon.cafe = @cafe
+    @menus = @cafe.menus
   end
 
 end
+
+#退会済み店舗は表示させない-------------
+  #def index
+  #  @cafes = Cafe.where(is_active: true)
+  #end
+
+  #def show
+    #unless Cafe.find(params[:id]).is_active
+      #redirect_to root_path, alert: "削除済みです"
+    #end
+  #end
+#----------------------------------
