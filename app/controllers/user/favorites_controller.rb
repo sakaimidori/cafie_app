a158525ed593c70980d0432834b033d7bffc8af5
sakaimidori <mdr20150526@gmail.com>
2022-10-19 11:22:33 +0000

    update:お気に入り機能のcreate、destroyアクションの中身を実装

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,8 +1,19 @@
 class User::FavoritesController < ApplicationController
 
   def create
+    cafe = Cafe.find(params[:cafe_id])
+
+    favorite = current_user.favorites.new(cafe_id: cafe.id)
+    favorite.save
+    flash[:notice] = "お気に入りに保存しました"
+    redirect_to cafe_path(cafe)
   end
 
   def destroy
+    cafe = Cafe.find(params[:cafe_id])
+    favorite = current_user.favorites.find_by(cafe_id: cafe.id)
+    favorite.destroy
+    flash[:notice] = "お気に入りから削除しました"
+    redirect_to cafe_path(cafe)
   end
 end
