5ace006f7faac85da3b11e1997707a12fc225f85
sakaimidori <mdr20150526@gmail.com>
2022-10-20 13:07:10 +0000

    update:Reviewテーブルのカラムを定義した

---
+++ db/migrate/20221019075848_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+
+      t.integer :user_id, null: false
+      t.integer :cafe_id, null: false
+      t.text :content, null: false
+
+      t.timestamps
+    end
+  end
+end
