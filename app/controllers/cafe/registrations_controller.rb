5d0385efc028e817293713ee36f8dadce8df21ac
sakaimidori <mdr20150526@gmail.com>
2022-11-02 11:09:08 +0000

    fix:店舗新規登録後の遷移画面を修正

--- app/controllers/cafe/registrations_controller.rb
+++ app/controllers/cafe/registrations_controller.rb
@@ -5,7 +5,7 @@ class Cafe::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
-    cafes_path
+    cafe_path
   end
 
 
