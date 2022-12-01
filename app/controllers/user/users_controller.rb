8fe8cf3223622a8d4146c0dd93e9b0240edfdc56
sakaimidori <mdr20150526@gmail.com>
2022-12-01 18:49:26 +0000

    update:不要なコードを削除

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -4,8 +4,6 @@ class User::UsersController < ApplicationController
 
   def show
     @user = current_user
-     #Cafe.where(is_active: true)
-    #cafe = Cafe.where(is_active: true)
     @favorites = @user.favorites
     @reviews = @user.reviews
   end
